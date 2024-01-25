ac2880afad5f88d75d221c57229d4e6d40e41c7b
Steven Oderayi <oderayi@gmail.com>
2024-01-25 10:52:49 +0100

    fix: fix workflow script

--- .github/workflows/scripts/update-charts.sh
+++ .github/workflows/scripts/update-charts.sh
@@ -27,7 +27,7 @@ while ! (grep -q "* Changed:	0" output.log); do
     echo -e "\nUpdating charts ... pass: $((++pass_count))\n"
     ./update-charts-dep.sh 
     find . -maxdepth 1 -type d -not -path '*/\.*' | sed 's/^\.\///g' | xargs -I {} helm repo index {} 
-    updatecli apply --config .github/workflows/manifests/second-pass |& tee output.log
+    updatecli apply --config .github/workflows/manifests/second-pass |& tee $dir/output.log
 done
 
 # Remove temporary file
